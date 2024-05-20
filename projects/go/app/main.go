0f761c6298dad18e52211d5d979efbf1d55d41bb
Ananth Kamath <infinitekamath@gmail.com>
2024-05-20 13:58:11 +0530

    Add first go app to the repo

---
+++ projects/go/app/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+
+	"github.com/gorilla/mux"
+	"github.com/ananthkamath/bazel-polyglot-monorepo/projects/go/greeting"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	w.Write([]byte(greeting.Greeting() + " You did it, you are amazing!!"))
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+
+	return port
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	// Bind to a port and pass our router in
+	port := getPort()
+	log.Println("running program's operating system target: " +  runtime.GOOS)
+	log.Println("running program's architecture target: " + runtime.GOARCH)
+	log.Println("Going to listen on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
